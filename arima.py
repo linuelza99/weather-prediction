c24b2af486c2b63283fd095a7f51a378779019fc
linuelza99 <linuelza99@gmail.com>
2019-11-13 16:06:34 +0530

    Added regression

--- arima.py
+++ arima.py
@@ -20,23 +20,54 @@ import time
 from datetime import datetime, timedelta
 import os
 
-df = pd.read_csv("weatherdata.csv").set_index("time")
+df = pd.read_csv("weather.csv").set_index("time")
+
+city = str(input("Enter city: "))
+#city = "Karnataka"
+df = df.loc[df['city'] == city]
+
 df.index = pd.to_datetime(df.index)
 #df.sort_values("time")
-
-feature = ['temperatureMin']
+predictfeature = str(input("Enter feature to predict: "))
+#predictfeature = "temperatureMax"
+feature = [predictfeature]
 
 data = df[feature]
 print(data)
 y = data
 
 # The 'MS' string groups the data in buckets by start of the month
-y = y.temperatureMin.resample('W').mean()
+
+if predictfeature == 'precipIntensity':
+    y = y.precipIntensity.resample('d').mean()
+elif predictfeature == 'precipIntensityMax':
+    y = y.precipIntensityMax.resample('d').mean()
+elif predictfeature == 'precipProbability':
+    y = y.precipProbability.resample('d').mean()
+elif predictfeature == 'temperatureMin':
+    y = y.temperatureMin.resample('d').mean()
+elif predictfeature == 'temperatureMax':
+    y = y.temperatureMax.resample('d').mean()
+else:
+    print("Invalid feature")
+    exit(1)  
 
 # The term bfill means that we use the value before filling in missing values
 y = y.fillna(y.bfill())
-
 print(y)
+
+temp = y.head(1)
+print(temp)
+temp = np.array(temp.index)
+print("temp = " , temp)
+
+lastdate = ''
+for i in  temp:
+    t = str(i).split("T")
+    t = t[0]
+    t = t.split("-") 
+    t= datetime(int(t[0]),int(t[1]),int(t[2])) 
+    lastdate = t
 
 y.plot(figsize=(15, 6))
 plt.show()
@@ -84,30 +115,30 @@ results = mod.fit()
 
 
 
-pred = results.get_prediction(start=pd.to_datetime('2017-02-12'), dynamic=False)
+pred = results.get_prediction(start=pd.to_datetime(lastdate), dynamic=False)
 pred_ci = pred.conf_int()
 print(pred.predicted_mean)
 
 
 y_forecasted = pred.predicted_mean
-y_truth = y['2017-02-12':]
+y_truth = y[lastdate:]
 
 # Compute the mean square error
 mse = ((y_forecasted - y_truth) ** 2).mean()
-print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
+print('The Mean Squared Error of our dynamic forecasts is {}'.format(round(mse, 2)))
 
-pred_dynamic = results.get_prediction(start=pd.to_datetime('2019-10-06'), dynamic=True, full_results=True)
+pred_dynamic = results.get_prediction(start=pd.to_datetime(lastdate), dynamic=True, full_results=True)
 pred_dynamic_ci = pred_dynamic.conf_int()
 
-ax = y['2017':].plot(label='observed', figsize=(20, 15))
-pred_dynamic.predicted_mean.plot(label='Dynamic Forecast', ax=ax)
+#ax = y['2017':].plot(label='observed', figsize=(20, 15))
+#pred_dynamic.predicted_mean.plot(label='Dynamic Forecast', ax=ax)
 print(pred_dynamic.predicted_mean)
 
 
 
 # Extract the predicted and true values of our time series
 y_forecasted = pred_dynamic.predicted_mean
-y_truth = y['2019-10-01':]
+y_truth = y[lastdate:]
 
 # Compute the mean square error
 mse = ((y_forecasted - y_truth) ** 2).mean()
@@ -126,8 +157,7 @@ ax.fill_between(pred_ci.index,
                 pred_ci.iloc[:, 0],
                 pred_ci.iloc[:, 1], color='k', alpha=.25)
 ax.set_xlabel('Date')
-ax.set_ylabel('Minimum temperature')
+ax.set_ylabel(predictfeature)
 
 plt.legend()
 plt.show()
-
--- regression.py
+++ regression.py
@@ -1,25 +1,16 @@
-# Python program to find current 
-# weather details of any city 
-# using openweathermap api 
-
-# import required modules 
-
-import matplotlib
-import matplotlib.pyplot as plt
+#import matplotlib
+#import matplotlib.pyplot as plt
 import numpy as np
-
 import pandas as pd
 import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, median_absolute_error
 from sklearn.model_selection import train_test_split
-
 import requests, json 
 import collections
 import time
 import datetime
 import os
-
 
 def get_target_date():
     """Return target date 1000 days prior to current date."""
@@ -32,14 +23,6 @@ def derive_nth_day_feature(df, feature, N):
     col_name = f'{feature}_{N}'
     df[col_name] = nth_prior_measurements
 
-
-"""
-features = [
-    "time", "precipIntensity","precipIntensityMax","precipProbability","dewPoint",
-    "humidity","pressure","windSpeed","cloudCover","visibility","temperatureMin","temperatureMax"]
-    """
-
-
 features = [
         'time', 'precipIntensity', 'precipIntensityMax',
         'precipProbability',
@@ -49,20 +32,20 @@ features = [
 ]
 
 df = pd.read_csv("weather.csv").set_index("time")
-df = df.loc[df['city'] == "Mangalore"]
+
+city = str(input("Enter city: "))
+
+df = df.loc[df['city'] == city]
+df.dropna()
 
 print(df.sort_values("time"))
 print(df.columns)
 
-
-
 nextday = datetime.datetime.today() 
-
+nextday += datetime.timedelta(days=1)
 temp = str(nextday).split(" ")[0]
 temp = (temp).split("-") 
-#print(temp)
 temp = datetime.datetime(int(temp[0]),int(temp[1]),int(temp[2]))
-#print(temp)
 
 nextday = temp
 print(nextday)
@@ -73,15 +56,7 @@ print(newdf)
 
 df.index = pd.to_datetime(df.index)
 newdf.index = pd.to_datetime(newdf.index)
-#print(df.sort_values)
-#print(df.columns)
 
-"""
-features = [
-    "precipIntensity","precipIntensityMax","precipProbability","dewPoint",
-    "humidity","pressure","windSpeed","cloudCover","visibility","temperatureMin","temperatureMax"
-]
-"""
 features = [
         'precipIntensity', 'precipIntensityMax',
         'precipProbability',
@@ -91,8 +66,6 @@ features = [
 ]
 
 data = df[features]
-#print(data)    
-
 
 data = data.sort_values(by=['time'])
 data = data.resample('d').mean().dropna(how='all')
@@ -101,17 +74,9 @@ data = data.append(newdf)
 
 df = data
 
-#print(data.loc[nextday])
-
-#print("Database: ", df)
-
-#tmp = df[['temperatureMean', 'dewPoint']].head(4)
-#print(tmp)
 # target measurement of mean temperature
-feature = 'temperatureMin'
-
-# total number of rows
-#rows = tmp.shape[0]
+predictfeature = str(input("Enter feature to predict: "))
+ft = [predictfeature]
 
 #print(tmp[feature][1])
 # a list representing Nth prior measurements of feature
@@ -120,14 +85,14 @@ feature = 'temperatureMin'
 
 for feature in features:
     if feature != 'time':
-	        for N in range(1, 4):
- 	           derive_nth_day_feature(df, feature, N)
+            for N in range(1, 4):
+               derive_nth_day_feature(df, feature, N)
 
-#print("Dataframe with nth day features: " , df)
+print("Dataframe with nth day features: " , df)
 
 to_remove = [
     feature for feature in features
-    if feature not in ['temperatureMin', 'temperatureMax']
+    if feature not in ft
 ]
 #print(to_remove)
 
@@ -148,7 +113,7 @@ spread = df.describe().T
 IQR = spread['75%'] - spread['25%']
 
 # create an outliers column which is either 3 IQRs below the first quartile or
-# 3 IQRs above the third quartile	
+# 3 IQRs above the third quartile   
 spread['outliers'] = (spread['min'] <(spread['25%'] -(3 * IQR))) | (spread['max'] > (spread['75%'] + 3 * IQR))
 #print(spread)
 #print(spread.iloc[spread.outliers,])
@@ -161,33 +126,29 @@ trial = df.loc[nextday]
 df = df.dropna()
 #print(df)
 
-df_corr = df.corr()[['temperatureMin']].sort_values('temperatureMin')
+df_corr = df.corr()[[predictfeature]].sort_values(predictfeature)
 #print(df_corr)   
-df_corr_fil = df_corr[abs(df_corr['temperatureMin']) > 0.40]
+df_corr_fil = df_corr[abs(df_corr[predictfeature]) > 0.30]
 #print(df_corr_fil)
 
-
-unwanted = ['temperatureMin', 'temperatureMax']
+unwanted = [predictfeature]
 predictors = df_corr_fil.index.tolist()
 predictors = [i for i in predictors if i not in unwanted]
-#print(predictors)
+print("Predictors: ", predictors)
 
-df2 = df[['temperatureMin'] + predictors]
-trial = trial[['temperatureMin'] + predictors]
-
+df2 = df[[predictfeature] + predictors]
+trial = trial[[predictfeature] + predictors]
 
 X = df2[predictors]
 trial = trial[predictors]
 
-y = df2['temperatureMin']
+y = df2[predictfeature]
 alpha = 0.05
 
 # Add a constant to the predictor variable set to represent the Bo intercept
 X = sm.add_constant(X)
 #print("Testing dataset: ", trial)
 #print("X dataset: ", X)
-
-
 
 def stepwise_selection(X,
                        y,
@@ -198,27 +159,25 @@ def stepwise_selection(X,
     included = list(initial_list)
     #print("Initial list : ", initial_list)
     
-
     while True:
-    	#print("List:", included)
-    	changed = False
-    	model = sm.OLS(y,X[included]).fit()
-    	# use all coefs except intercept
-    	pvalues = model.pvalues.iloc[1:]
-    	#print("Values: ", pvalues)
-    	worst_pval = pvalues.max()  # null if pvalues is empty
-    	if worst_pval > threshold_out:
-    		changed = True
-    		worst_feature = pvalues.idxmax()
-    		#print("Worst Feature:", worst_feature)
-    		included.remove(worst_feature)
-    		#print("List:", included)
-    		if verbose:
-    			print('Drop {:30} with p-value {:.6}'.format(worst_feature, worst_pval))
-    	if not changed:
-    		break
+        #print("List:", included)
+        changed = False
+        model = sm.OLS(y,X[included]).fit()
+        # use all coefs except intercept
+        pvalues = model.pvalues.iloc[1:]
+        #print("Values: ", pvalues)
+        worst_pval = pvalues.max()  # null if pvalues is empty
+        if worst_pval > threshold_out:
+            changed = True
+            worst_feature = pvalues.idxmax()
+            #print("Worst Feature:", worst_feature)
+            included.remove(worst_feature)
+            #print("List:", included)
+            if verbose:
+                print('Drop {:30} with p-value {:.6}'.format(worst_feature, worst_pval))
+        if not changed:
+            break
     return included
-
 
 result = stepwise_selection(X, y)
 
@@ -244,7 +203,6 @@ prediction = regressor.predict(X_test)
 #print("X_test : " , X_test)
 
 #print("Prediction: ", prediction)
-
 
 trial = [trial]
 print(trial)
